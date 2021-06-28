3d8167fe1ca867212b57c73ae64d6dcd9a6bdabb
Shubham Pal <shubham.mca19.du@gmail.com>
2021-06-28 19:58:49 +0530

    News cards with news articles

--- src/components/NewsCards/NewsCards.js
+++ src/components/NewsCards/NewsCards.js
@@ -4,8 +4,58 @@ import { Grid, Grow, Typography } from '@material-ui/core'
 import useStyles from './styles'
 import NewsCard from '../NewsCard/NewsCard'
 
+const infoCards = [
+  {
+    color: '#00838f',
+    title: 'Latest News',
+    text: 'Give me the latest news'
+  },
+  {
+    color: '#1565c0',
+    title: 'News by Categories',
+    info: 'Business, Entertainment, General, Health, Science, Technology, Gaming',
+    text: 'Give me the latest news'
+  },
+  {
+    color: '#4527a0',
+    title: 'News by Terms',
+    info: 'Windows, Playstation 5, Smartphone etc.',
+    text: 'Give me the latest news'
+  },
+  {
+    color: '#283593',
+    title: 'News by Sources',
+    info: 'CNN, BBC News, Time, ',
+    text: ''
+  },
+]
+
 const NewsCards = ({ articles }) => {
   const classes = useStyles()
+
+  if (!articles.length) {
+    return (
+      <Grow in>
+        <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+          {infoCards.map((infoCard) => (
+            <Grid item xs={12} sm={6} md={4} lg={3} className={ classes.infoCard }>
+              <div className={ classes.card } style={{ backgroundColor: infoCard.color }}>
+                <Typography variant="h5">{ infoCard.title }</Typography>
+                { 
+                  infoCard.info ? 
+                    (<Typography variant="h6">
+                      <strong>{ infoCard.title.split(' ')[2] }:</strong><br />
+                      {infoCard.info}
+                    </Typography>) : null 
+                }
+                <Typography variant="h6">Try saying: <br /><i>{ infoCard.text }</i></Typography>
+              </div>
+            </Grid>
+          ))}
+        </Grid>
+      </Grow>
+    )
+  }
 
   return (
     <Grow in>
